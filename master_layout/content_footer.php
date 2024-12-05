f2866e35dc6ce7a953d01fb67e0ca51cd87016d4
chinhhoangtc <161810260+chinhhoangtc@users.noreply.github.com>
2024-12-05 21:24:17 +0700

    cmt

--- master_layout/content_footer.php
+++ master_layout/content_footer.php
@@ -1,22 +1,117 @@
-
 <!-- Footer start -->
 <footer>
   <div class="top-sec">
-    <div class="container ">
-      <div class="row match-height-container">
+    <div class="container border">
+      <div class="row">
+        <!-- First Column: Hà Nội Address -->
         <div class="col-sm-6 subscribe-info wow fadeInDown animated" data-wow-delay="1s" data-wow-offset="40">
-          <div class="row">
+          <h4>Trụ sở Hà Nội</h4>
+          <p>
+            Tầng 21, Tòa nhà Center Building, Hapulico Complex, số 1 Nguyễn Huy Tưởng, phường Thanh Xuân Trung, quận Thanh Xuân, Hà Nội.<br>
+            Điện thoại: <strong>024 7309 5555</strong>, máy lẻ 62.370
+          </p>
+        </div>
 
+        <!-- Second Column: Media & Advertising -->
+        <div class="col-sm-6 subscribe-info wow fadeInDown animated" data-wow-delay="1s" data-wow-offset="40">
+          <h4>Hợp tác Truyền thông</h4>
+          <p>
+            Điện thoại: <strong>024 7309 5555 (máy lẻ 62.370)</strong><br>
+            Email: <a href="mailto:marketing@abc.vn">marketing@abc.vn</a><br><br>
+            <strong>Liên hệ Quảng cáo:</strong><br>
+            Điện thoại: <strong>024 7300 7108</strong><br>
+            Email: <a href="mailto:giaitrixahoi@admicro.vn">giaitrixahoi@admicro.vn</a>
+          </p>
         </div>
       </div>
     </div>
   </div>
+
   <div class="btm-sec">
     <div class="container">
       <div class="row">
-        <div class="col-sm-16">
+        <div class="col-sm-12">
+          <p>
+            © Copyright 2007 - 2024 – Công ty Cổ phần ABC<br>
+            Tầng 17, 19, 20, 21 Tòa nhà Center Building - Hapulico Complex, Số 1 Nguyễn Huy Tưởng, Thanh Xuân, Hà Nội.<br>
+            Giấy phép thiết lập trang thông tin điện tử tổng hợp trên mạng số 2215/GP-TTĐT do Sở Thông tin và Truyền thông Hà Nội cấp ngày 10 tháng 4 năm 2019
+          </p>
         </div>
       </div>
     </div>
   </div>
 </footer>
+
+<style>
+  footer {
+  font-family: Arial, sans-serif;
+  background-color: #f8f8f8;
+  padding: 30px 0;
+  color: #333;
+}
+
+footer .top-sec {
+  background-color: #3d566e;
+  border-bottom: 1px solid #ddd;
+  padding-bottom: 20px;
+}
+
+footer .btm-sec {
+  background-color: #333;
+  color: #fff;
+  padding-top: 20px;
+  padding-bottom: 10px;
+}
+
+footer h4 {
+  font-size: 18px;
+  margin-bottom: 10px;
+  font-weight: bold;
+}
+
+footer p {
+  font-size: 14px;
+  line-height: 1.6;
+}
+
+footer a {
+  color: #1a73e8;
+  text-decoration: none;
+}
+
+footer a:hover {
+  text-decoration: underline;
+}
+
+footer .container {
+  max-width: 1140px;
+  margin: 0 auto;
+}
+
+footer .col-sm-6 {
+  margin-bottom: 20px;
+}
+
+footer .row {
+  display: flex;
+  justify-content: space-between;
+}
+
+footer .col-sm-12 {
+  text-align: center;
+}
+
+footer .border {
+  /* border: 1px solid #ddd; */
+}
+
+footer .subscribe-info {
+  padding: 15px;
+  border-right: 1px solid #ddd;
+}
+
+footer .subscribe-info:last-child {
+  border-right: none;
+}
+
+</style>
